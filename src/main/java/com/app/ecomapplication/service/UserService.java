a1565d57a67305807dfe1f5d5afab088a756fc1c
Ashish-Agarwal-12 <agarwalasish567@gmail.com>
2026-02-20 06:51:02 +0530

    feat: Added DTO Pattern For UserRequest & UserResponse

--- src/main/java/com/app/ecomapplication/service/UserService.java
+++ src/main/java/com/app/ecomapplication/service/UserService.java
@@ -1,5 +1,9 @@
 package com.app.ecomapplication.service;
 
+import com.app.ecomapplication.dto.AddressDTO;
+import com.app.ecomapplication.dto.UserRequest;
+import com.app.ecomapplication.dto.UserResponse;
+import com.app.ecomapplication.model.Address;
 import com.app.ecomapplication.model.User;
 import com.app.ecomapplication.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,32 +20,65 @@ public class UserService {
     private final UserRepository userRepository;
     // private Long nextId = 1L;
 
-    public List<User> fetchAllUsers() {
-        return userRepository.findAll();
+    private UserResponse mapToUserResponse(User user) {
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(String.valueOf(user.getId()));
+        userResponse.setEmail(user.getEmail());
+        userResponse.setPhone(user.getPhone());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getFirstName());
+        userResponse.setRole(user.getRole());
+
+        if(user.getAddress() != null) {
+            AddressDTO addressDTO = new AddressDTO();
+            addressDTO.setStreet(user.getAddress().getStreet());
+            addressDTO.setCity(user.getAddress().getCity());
+            addressDTO.setState(user.getAddress().getState());
+            addressDTO.setZipCode(user.getAddress().getZipCode());
+            addressDTO.setCountry(user.getAddress().getCountry());
+            userResponse.setAddress(addressDTO);
+        }
+        return userResponse;
     }
 
-    public Optional<User> getUserById(Long id) {
-        return userRepository.findById(id);
+    private void updateUserFromRequest(User user, UserRequest userRequest) {
+        user.setEmail(userRequest.getEmail());
+        user.setPhone(userRequest.getPhone());
+        user.setFirstName(userRequest.getFirstName());
+        user.setLastName(userRequest.getFirstName());
+
+        if(userRequest.getAddress() != null) {
+            Address address = new Address();
+            address.setStreet(userRequest.getAddress().getStreet());
+            address.setCity(userRequest.getAddress().getCity());
+            address.setState(userRequest.getAddress().getState());
+            address.setZipCode(userRequest.getAddress().getZipCode());
+            address.setCountry(userRequest.getAddress().getCountry());
+            user.setAddress(address);
+        }
     }
 
-    public void createUser(User user) {
+    public List<UserResponse> fetchAllUsers() {
+        return userRepository.findAll().stream()
+                .map(this::mapToUserResponse)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<UserResponse> getUserById(Long id) {
+        return userRepository.findById(id)
+                .map(this::mapToUserResponse);
+    }
+
+    public void createUser(UserRequest userRequest) {
+        User user = new User();
+        updateUserFromRequest(user, userRequest);
         userRepository.save(user);
     }
 
-    public Boolean updateUser(Long id, User updatedUser) {
-//        return userList.stream()
-//                .filter(user -> user.getId().equals(id))
-//                .findFirst()
-//                .map(existingUser -> {
-//                    existingUser.setFirstName(updatedUser.getFirstName());
-//                    existingUser.setLastName(updatedUser.getLastName());
-//                    return true;
-//                }).orElse(false);
-//    }
+    public Boolean updateUser(Long id, UserRequest updatedUser) {
         return userRepository.findById(id)
                 .map(existingUser -> {
-                    existingUser.setFirstName(updatedUser.getFirstName());
-                    existingUser.setLastName(updatedUser.getLastName());
+                    updateUserFromRequest(existingUser, updatedUser);
                     userRepository.save(existingUser);
                     return true;
                 }).orElse(false);
